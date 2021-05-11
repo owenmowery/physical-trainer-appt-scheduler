370c858ece2493a537a27e87a9685571328f25e7
Owen Mowery <owenmowery99@gmail.com>
2021-05-11 13:34:00 -0400

    login authentication successfull

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,18 +11,12 @@ class SessionsController < ApplicationController
 
     def login
         @client = Client.find_by(client_email: params[:client_email])
-        binding.pry
-        if @client && @client.authenticate(client_params[:password])
+        if @client && @client.authenticate(params[:password])
             session[:client_id] = @client.id
             redirect_to clients_path
         else
-            #redirect_to client_login_path
+            redirect_to client_login_path
         end
     end
 
-    private
-
-    def client_params
-        params.permit(:name, :client_email, :age, :height, :weight, :goal, :password, :password_confirmation)
-    end
 end
