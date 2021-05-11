116b78390a5cbb03186f8a0ef52351bc2a4926bb
Owen Mowery <owenmowery99@gmail.com>
2021-05-11 00:18:48 -0400

    form submits, creates client account, and renders clients#index form

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -3,6 +3,11 @@ class ClientsController < ApplicationController
     end
 
     def create
-    `
+        @client = Client.new(client_params)
+        render 'clients/index'
+    end
+
+    def client_params
+        params.permit(:name, :client_email, :age, :height, :weight, :goal, :password_digest)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   get '/client_signup', to: 'clients#signup'
+  post '/client_signup', to: 'clients#create'
   get '/trainer_signup', to: 'trainers#signup'
   resources :appointments
   resources :clients
